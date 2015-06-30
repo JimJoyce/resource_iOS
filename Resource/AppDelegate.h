b0bb451922690a7f587e55c14b8728d7ecc63f18
JimJoyce <j.joyce92@gmail.com>
2015-06-30 00:31:44 -0500

    Initial Commit

---
+++ Resource/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Resource
+//
+//  Created by Jim Joyce on 6/30/15.
+//  Copyright (c) 2015 JimJoyce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Resource/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Resource
+//
+//  Created by Jim Joyce on 6/30/15.
+//  Copyright (c) 2015 JimJoyce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
