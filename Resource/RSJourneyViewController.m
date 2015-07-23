6d996e3680d8507c69bcd7311d352ad56fd006d8
Jim Joyce <j.joyce92@gmail.com>
2015-07-23 02:57:25 -0500

    no changes just rebuilt the app

--- Resource/RSJourneyViewController.m
+++ Resource/RSJourneyViewController.m
@@ -30,12 +30,13 @@
                                                                             style:UIBarButtonItemStylePlain
                                                                            target:nil
                                                                            action:nil];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemAdd
+                                                                                          target:self
+                                                                                          action:@selector(addNewJourney)];
+}
+
+-(void)addNewJourney {
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning {
